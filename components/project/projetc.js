a11abfea526b9e38f24c47a6dd51bfd6657e781f
sarwar-asik <shasik2021@gmail.com>
2022-12-22 21:00:56 +0600

    projects added

---
+++ components/project/projetc.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const projetc = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default projetc;
--- pages/projects.js
+++ pages/projects.js
@@ -1,9 +1,42 @@
 import React from "react";
 
-const projects = () => {
+const projects = ({ projects }) => {
+  console.log(projects);
+
   return (
-    <div>
-      <h1 className="text-3xl mt-[100px]"> My Project</h1>
+    <div className="w-[80%] mx-auto">
+      <h1 className="text-3xl text-center  my-2 font-bold mt-[100px]">
+        {" "}
+        My Project
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {projects.map((project) => {
+          const { img, name } = project;
+          return (
+            <>
+              <div
+                className="card w-[90%]
+              mx-auto bg-base-100 shadow-xl "
+              >
+                <figure>
+                  <img src={img} className="h-[19rem] w-full" />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title">
+                    {name}
+                    <div className="badge badge-info">NEW</div>
+                  </h2>
+                  <p>If a dog chews shoes whose shoes does he choose?</p>
+                  <div className="card-actions justify-end">
+                    <div className="badge badge-outline">Fashion</div>
+                    <div className="badge badge-outline">Products</div>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 };
@@ -12,11 +45,11 @@ export default projects;
 
 export const getStaticProps = async () => {
   const res = await fetch(`https://sarwar-hossain-server.vercel.app/projects`);
-  const data = res.json();
-console.log(data);
+  const data = await res.json();
+  // console.log(data);
   return {
     props: {
-      project: data
+      projects: data,
     },
   };
 };
